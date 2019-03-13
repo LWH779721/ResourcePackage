f3e8299484ae33596ada8506ad6ea7cecf355348
liaowenhua <liaowenhua@hcn2000.com>
2019-03-13 12:39:08 +0800

    切换成使用cmake编译

--- test/package.cpp
+++ test/package.cpp
@@ -6,13 +6,11 @@ using namespace std;
 int main(int argc, char **args)
 {
 	ResourcePackage package1("music");
-#if 0	
+	
 	package1.dump();
 	package1.init("");
-	package1.append("snowboy.wav");
+	package1.append(args[1]);
 	package1.DPackage();
-#endif	
-	package1.Unpackage("/home/lwh/workspace/ResourcePackage/src/music.so", "snowboy_wav");
 	
 	return 0;
 }
